7614d1fec41ce6e5332267d0cf95aeed4196eef8
Milad Vafaeifard <milad.vafaeifard@gmail.com>
2025-03-21 21:56:12 +0100

    fix: remove manifest.json and update rsbuild configuration for improved asset handling

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,18 +1,25 @@
-import { defineConfig } from '@rsbuild/core'
-import { pluginReact } from '@rsbuild/plugin-react'
+import { defineConfig } from '@rsbuild/core';
+import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
   html: {
-    template: 'public/index.html',
+    template: './public/index.html',
+    favicon: './public/favicon.ico',
+    appIcon: {
+      name: 'React Todo App DexieJs',
+      icons: [
+        { src: './public/logo192.png', size: 192 },
+        { src: './public/logo512.png', size: 512 },
+      ],
+    },
   },
-  // root: process.env.NODE_ENV === 'production' ? './react-todo-app-dexiejs' : './',
   server: {
     base: process.env.NODE_ENV === 'production' ? '/react-todo-app-dexiejs' : '/',
   },
   output: {
     distPath: {
-      root: process.env.NODE_ENV === 'production' ? './build' : './dist'
-    }
-  }
-})
+      root: process.env.NODE_ENV === 'production' ? './build' : './dist',
+    },
+  },
+});
